3260df0d80390a694d8e158a06f74f7cdb88fd65
Thomas Maxwell <thomas.maxwell@nasa.gov>
2014-01-13 12:01:57 -0500

    INitial setup.

---
+++ HCI_Browser/SliceWidget.py
@@ -0,0 +1,17 @@
+'''
+Created on Jan 13, 2014
+
+@author: tpmaxwel
+'''
+
+class SliceWidget(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__( self, params ):
+        '''
+        Constructor
+        '''
+        
